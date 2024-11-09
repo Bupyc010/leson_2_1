8cd57ffd5c359149b72bf0a594308d34f23632b2
Ruslan <112889383+Bupyc010@users.noreply.github.com>
2024-11-09 12:27:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+#1st program
+count_d = 12
+count_h = 1.5
+names_kurse = "Python"
+
+print(f"Курс: {names_kurse}, всего задач: {count_d},затрачено часов: {count_h}, среднее время выполнения: {count_h/count_d}")
