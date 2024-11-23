782c88d6a4b148eb360c7cd9f7680ab702c9d3f8
Ruslan <112889383+Bupyc010@users.noreply.github.com>
2024-11-23 15:29:02 +0200

    Add files via upload

---
+++ module_2_6.py
@@ -0,0 +1,20 @@
+def generator_password(n):
+    result = ''
+
+    for i in range(1, n + 1):
+        for j in range(i, n + 1):
+            pair_sum = i + j
+            if n % pair_sum == 0:
+                result += f"{i}{j}"
+    return result
+
+
+while True:
+    n = int(input('Ведите число от 3 до 20: '))
+    if 3 <= n <= 20:
+        break
+    else:
+        print('Введите правельное число.')
+
+print(f"Нужный пароль для числа {n}: " + generator_password(n))
+
