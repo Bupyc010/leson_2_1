f2d97f650e97b0ed35e73d3680eedba78d7efb9b
Ruslan <112889383+Bupyc010@users.noreply.github.com>
2024-11-24 21:11:03 +0200

    Add files via upload

--- module_2_6.py
+++ module_2_6.py
@@ -2,7 +2,7 @@ def generator_password(n):
     result = ''
 
     for i in range(1, n + 1):
-        for j in range(i, n + 1):
+        for j in range(i + 1, n + 1):
             pair_sum = i + j
             if n % pair_sum == 0:
                 result += f"{i}{j}"
