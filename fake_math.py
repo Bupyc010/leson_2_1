8714bdec843301db2b9b5cc1dce617f01e8fe16a
Ruslan <112889383+Bupyc010@users.noreply.github.com>
2024-12-07 16:09:42 +0200

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide (ferst, second):
+    if second != 0:
+        return round(ferst/second, 2)
+    else:
+        return 'Ошибка'
---
+++ true_math.py
@@ -0,0 +1,6 @@
+def divide (ferst, second):
+    if second != 0:
+        return ferst/second
+    else:
+        from math import inf
+        return float('inf')
