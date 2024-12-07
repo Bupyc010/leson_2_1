894cbceae560b13e88a6b3829f06d5e9fad1d614
Ruslan <112889383+Bupyc010@users.noreply.github.com>
2024-12-07 16:10:25 +0200

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,6 @@
+import module_4_1.fake_math as fake_m
+import module_4_1.true_math as true_m
+
+print(fake_m.divide(int(input('Введите первое число: ')), int(input('Введите второе число: '))))
+print()
+print(true_m.divide(int(input('Введите первое число: ')), int(input('Введите второе число: '))))
